ccb586e0f280b2e44f1aad5853197f10056e6b10
elena <develenar@gmail.com>
2023-12-12 22:02:10 +0900

    Add keywords

---
+++ keywords/example1.js
@@ -0,0 +1,3 @@
+// in Browser
+
+var name = "Elena"; // var 라는 단어는 변수를 선언할 때 사용하는 키워드입니다.
---
+++ keywords/example2.js
@@ -0,0 +1,4 @@
+// in Browser
+
+var return = '변수명'; // return 은 예약어라 변수명으로 사용할 수 없습니다.
+function for() {} // for 는 예약어라 함수명으로 사용할 수 없습니다.
