9e8b6df33f49b308af92db27d41f0c1673e3ea2d
elena <develenar@gmail.com>
2023-12-12 22:03:46 +0900

    Add hoisting

---
+++ hoisting/example3.js
@@ -0,0 +1,29 @@
+console.log(name); //값: undefined
+
+name = 'Elena';
+
+console.log(name); //값: Elena
+
+var name;
+
+//
+
+console.log(name); //값: undefined
+
+name = 'Elena';
+
+console.log(name); //값: Elena
+
+var name = "Eunhwa";
+
+//
+
+var name;
+
+console.log(name); //값: 
+
+name = 'Elena';
+
+console.log(name); //값: 
+
+name = "Eunhwa";
---
+++ hoisting/example4.js
@@ -0,0 +1,7 @@
+console.log(name); //값: name is not defined (hoisting 현상이 일어나지 않음)
+
+name = 'Elena';
+
+console.log(name);
+
+let name;
