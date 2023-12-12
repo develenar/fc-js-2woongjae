502e8b14095f477916249bf0a7fed46073871a4d
elena <develenar@gmail.com>
2023-12-12 22:06:58 +0900

    Add conditional

---
+++ conditional/example3.js
@@ -0,0 +1,27 @@
+// false
+// 0
+// ''
+// null
+// undefined
+// NaN
+
+if (false) console.log(false); //값: 
+if (0) console.log(false); //값: 
+if ('') console.log(''); //값: 
+if (null) console.log(null); //값: 
+if (undefined) console.log(undefined); //값: 
+if (NaN) console.log(NaN); //값: 
+
+// true
+// 37
+// -37
+// 'Elena'
+// {}
+// []
+
+if (true) console.log(true); //값: true
+if (37) console.log(37); //값: 37
+if (-37) console.log(-37); //값: -37
+if ('Elena') console.log('Elena'); //값: Elena
+if ({}) console.log({}); //값: {}
+if ([]) console.log([]); //값: []
